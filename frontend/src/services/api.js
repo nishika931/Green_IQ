da401b212b13e2a2e6a578186509bbc0b2ff060a
nishika931 <sahuunishika@gmail.com>
2026-07-07 13:22:50 +0530

    connecting with backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://green-iq-wi2s.onrender.com",
+  baseURL: "http://127.0.0.1:8000",
 });
 
 API.interceptors.request.use((config) => {
@@ -16,3 +16,4 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
+// https://green-iq-wi2s.onrender.com
